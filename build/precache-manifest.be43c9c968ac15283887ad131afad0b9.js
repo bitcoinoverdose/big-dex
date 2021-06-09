d04999b43c6c36c086ee85099ec835a7dbc52809
bitcoinoverdose <58384123+bitcoinoverdose@users.noreply.github.com>
2021-06-08 20:06:41 -0500

    0.2
    update as of 8:05 6/8/21

---
+++ build/precache-manifest.be43c9c968ac15283887ad131afad0b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081d8b2152548af44c823fe46ba73e2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b517410236ff85c9bd2b",
+    "url": "/static/css/2.3a0ac4c9.chunk.css"
+  },
+  {
+    "revision": "1fd7e044109a478a8598",
+    "url": "/static/css/main.c342341c.chunk.css"
+  },
+  {
+    "revision": "b517410236ff85c9bd2b",
+    "url": "/static/js/2.823c178c.chunk.js"
+  },
+  {
+    "revision": "583f3889ab5ba28ba005a8fb02907b46",
+    "url": "/static/js/2.823c178c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd7e044109a478a8598",
+    "url": "/static/js/main.2a4512be.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  },
+  {
+    "revision": "d6bfdb3d8def15b0cb790c039c1cd55a",
+    "url": "/static/media/logo.d6bfdb3d.svg"
+  }
+]);
