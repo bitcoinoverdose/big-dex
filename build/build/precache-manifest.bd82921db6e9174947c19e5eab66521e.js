874a87fab7130fb87d374c191c535c50bf466ad9
bitcoinoverdose <58384123+bitcoinoverdose@users.noreply.github.com>
2021-06-15 12:27:07 -0500

    Build Big DEX 0.4.1
    CSS changes and Link Build

---
+++ build/build/precache-manifest.bd82921db6e9174947c19e5eab66521e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e561b009968ae12b484efa1d606ee13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a08baee597c7cbd4816c",
+    "url": "/static/css/2.3a0ac4c9.chunk.css"
+  },
+  {
+    "revision": "5c62771857b8a27b6054",
+    "url": "/static/css/main.c342341c.chunk.css"
+  },
+  {
+    "revision": "a08baee597c7cbd4816c",
+    "url": "/static/js/2.ffc2f0c1.chunk.js"
+  },
+  {
+    "revision": "583f3889ab5ba28ba005a8fb02907b46",
+    "url": "/static/js/2.ffc2f0c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c62771857b8a27b6054",
+    "url": "/static/js/main.c0c8ff56.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  },
+  {
+    "revision": "d6bfdb3d8def15b0cb790c039c1cd55a",
+    "url": "/static/media/logo.d6bfdb3d.svg"
+  }
+]);
